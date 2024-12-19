ed8256b68c8e9553be6e39addfbba8b5cf3cbfc8
thameem-cavininfotech <thameem.s@cavininfotech.com>
2024-12-20 02:47:13 +0530

    feat: suspense lazy loading added

--- fintech_frontend_react/src/routes/index.tsx
+++ fintech_frontend_react/src/routes/index.tsx
@@ -1,53 +1,57 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { ROUTES } from "./RoutePaths.ts";
 import PrivateRoute from "./PrivateRoute.tsx";
 import PublicRoute from "./PublicRoute.tsx";
-import Home from "../components/Pages/Home/index.tsx";
-import Create from "../components/Pages/About/Create/index.tsx";
-import Login from "../components/Pages/Auth/Login/index.tsx";
-import Dashboard from "../components/Pages/Dashboard/index.tsx";
-import Settings from "../components/Pages/Settings/index.tsx";
-import NotFoundPage from "../components/Pages/NotFound/index.tsx";
+
+const Home = lazy(() => import("../components/Pages/Home/index.tsx"));
+const Create = lazy(() => import("../components/Pages/About/Create/index.tsx"));
+const Login = lazy(() => import("../components/Pages/Auth/Login/index.tsx"));
+const Dashboard = lazy(() => import("../components/Pages/Dashboard/index.tsx"));
+const Settings = lazy(() => import("../components/Pages/Settings/index.tsx"));
+const NotFoundPage = lazy(() => import("../components/Pages/NotFound/index.tsx"));
+
+const Loader = () => <div>Loading...</div>;
+
 const AppRoutes = () => {
   return (
     <Router>
-      <Routes>
-        {/* Public Routes... */}
-        <Route path={ROUTES.HOME} element={<Home />} />
-        <Route path={ROUTES.ABOUT} element={<Create />} />
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          {/* Public Routes... */}
+          <Route path={ROUTES.HOME} element={<Home />} />
+          <Route path={ROUTES.ABOUT} element={<Create />} />
 
-        {/* Protected Routes... */}
-        <Route
-          path={ROUTES.LOGIN}
-          element={
-            <PublicRoute>
-              <Login />
-            </PublicRoute>
-          }
-        />
+          {/* Protected Routes... */}
+          <Route
+            path={ROUTES.LOGIN}
+            element={
+              <PublicRoute>
+                <Login />
+              </PublicRoute>
+            }
+          />
+          <Route
+            path={ROUTES.DASHBOARD}
+            element={
+              <PrivateRoute>
+                <Dashboard />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path={ROUTES.SETTINGS}
+            element={
+              <PrivateRoute>
+                <Settings />
+              </PrivateRoute>
+            }
+          />
 
-        {/* Private Routes for Only accessible to authenticated users... */}
-        <Route
-          path={ROUTES.DASHBOARD}
-          element={
-            <PrivateRoute>
-              <Dashboard />
-            </PrivateRoute>
-          }
-        />
-
-        <Route
-          path={ROUTES.SETTINGS}
-          element={
-            <PrivateRoute>
-              <Settings />
-            </PrivateRoute>
-          }
-        />
-        {/* Page is not found  */}
-        <Route path={ROUTES.NOT_FOUND} element={<NotFoundPage />} />
-      </Routes>
+          {/* 404 Not Found Page... */}
+          <Route path={ROUTES.NOT_FOUND} element={<NotFoundPage />} />
+        </Routes>
+      </Suspense>
     </Router>
   );
 };
